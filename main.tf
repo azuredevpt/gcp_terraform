5b947d36598c42df35644be105d8058355f72e8e
anonymedevs <azuredevpt@guerrillamail.com>
2019-06-09 23:13:47 +0200

    add variables file

--- main.tf
+++ main.tf
@@ -1,9 +1,31 @@
+  variable project{
+   default = 
+   "us-central1-c"	
+ }
+
+  variable region{
+   default = 
+   "us-central1-c"	
+ }
+
+ variable zone{
+   description = "zone google du projet"
+   type = "string"
+   default = "us-central1-c"	
+ }
+
  provider "google" {
   credentials = "${file("credentials.json")}"
-  project     = "hadoop-hdp-239118"
-  region      = "us-central1"
-  zone      = "us-central1-c"	
+  project     = "${var.project}"	
+  region      = "${var.region}"	
+  zone      = "${var.zone}"	
 }
+
+ variable ports_inbounds{
+   description = "ports accéssibles en entrée"
+   type = "list"
+   default =["22","80"]
+ }
 
 #provider "aws" {
 #  region      = "eu-west-3"
@@ -57,7 +79,7 @@ resource "google_compute_firewall" "default" {
 
   allow {
     protocol = "tcp"
-    ports    = ["22", "80"]
+    ports    = "${var.ports_inbounds}"
   }
 
 }
